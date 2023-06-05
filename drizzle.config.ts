cc0b08e9c39b165872d9ec50895bb1e452ee31db
Joscha <neske.joscha@gmail.com>
2023-06-05 11:33:17 +0200

    remove silly imports

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from 'drizzle-kit'
 
-// import 'dotenv/config'
-
 export default {
   schema: './src/lib/db/schema.ts',
   out: './src/lib/db/migrations',
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,5 @@
 import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import { connect } from '@planetscale/database'
-// import { fetch } from 'undici'
 
 // create database connection
 const connection = connect({
