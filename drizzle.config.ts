9a06692f49ecbb5ed95eaba38ab36908d2273f38
tuffstuff <57072903+tuffstuff9@users.noreply.github.com>
2023-08-05 14:01:58 -0400

    Update drizzle.config.ts
    changed config to work with latest version of drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from 'drizzle-kit';
-import 'dotenv/config';
+import type { Config } from 'drizzle-kit'
+import 'dotenv/config'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL environment variable is required.');
